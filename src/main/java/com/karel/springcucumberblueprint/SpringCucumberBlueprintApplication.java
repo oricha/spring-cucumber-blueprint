2879728edc6c96ad1b2bd011c9bf93d1ce1b1504
Karel Muñiz <oricha@gmail.com>
2022-08-17 22:25:54 +0200

    More tests for Cucumber template

---
+++ src/main/java/com/karel/springcucumberblueprint/SpringCucumberBlueprintApplication.java
@@ -0,0 +1,13 @@
+package com.karel.springcucumberblueprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCucumberBlueprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCucumberBlueprintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karel/springcucumberblueprint/SpringCucumberBlueprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karel.springcucumberblueprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCucumberBlueprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
